a84bc6107019688aada2634c02ec0a75357e705a
Khalid Nahary <khalid@Khalids-MacBook-Pro.local>
2021-01-03 14:48:12 +0000

    added js functionality for input

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input');
 const error = document.querySelector('.error');
 const errorMessage = document.querySelector('.error--message')
 const formContainer = document.querySelector('form');
-
+const button = document.querySelector('button');
 
 
 
@@ -13,10 +13,25 @@ const formContainer = document.querySelector('form');
 
 const isValid  = (email) => {
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
-      return console.log('mecrovat')
-
+      error.classList.remove('display--off')
+      errorMessage.classList.remove('display--off')
+      formContainer.classList.remove('border--toggle')
     }
-} 
+    else if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+      error.classList.add('display--off')
+      errorMessage.classList.add('display--off')
+      formContainer.classList.add('border--toggle')
+    }
+    }
 
+const incorrectEmail = (i) => {
+  i.preventDefault();
+  if (input.value != ''){
+    isValid(input.value)
+    input.value = ''
+  }
+}
+
+button.addEventListener('click', incorrectEmail)
 
 
